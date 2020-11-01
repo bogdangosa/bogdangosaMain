531aa3c1cb37006dec68217e406888d2e9b381cc
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2020-11-01 10:19:01 +0200

    added hamburger made mobile version better

--- MicsunelPortofolio/script.js
+++ MicsunelPortofolio/script.js
@@ -1,12 +1,32 @@
 const Header = document.getElementById("Header")
+const Hamburger = document.getElementById("hamburger")
+const Nav = document.getElementById("nav_links")
 let lastScroll=0
+let ContainedNav=false
 
 
 window.addEventListener("scroll",function(){
     let Scroll= window.pageYOffset
-    if(Scroll>lastScroll)
+    if(Scroll>lastScroll){
         Header.style.top="-10vh"
-    else
+        if(Nav.classList.contains("visible")){
+                Nav.classList.remove("visible")
+                ContainedNav=true
+        }
+    }
+    else{
         Header.style.top="0"
+        if(ContainedNav){
+            Nav.classList.add("visible")  
+            ContainedNav = false
+        }     
+    }
     lastScroll=Scroll;
+})
+
+Hamburger.addEventListener("click",function(){
+    const lines = document.getElementsByClassName('line')
+    Nav.classList.toggle("visible")
+    for(let i=0;i < lines.length;i++)
+        lines[i].classList.toggle("whitebg")
 })

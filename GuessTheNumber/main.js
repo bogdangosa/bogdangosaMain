87b9242f5ae687b79fd6d59540a41aa8b2ff30d2
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2020-11-06 13:48:19 +0200

    guess the number v2

--- GuessTheNumber/main.js
+++ GuessTheNumber/main.js
@@ -1,32 +1,46 @@
-
-const StartBtn = document.getElementById("startBtn");
-const Guess = document.getElementById("guess");
-const HiLoContainer = document.getElementById("HiLoContainer");
-const HigherBtn = document.getElementById("hiBtn");
-const LowerBtn = document.getElementById("loBtn");
+const StartBtn = document.getElementById("startBtn")
+const Guess = document.getElementById("guess")
+const HiLoContainer = document.getElementById("HiLoContainer")
+const HigherBtn = document.getElementById("hiBtn")
+const LowerBtn = document.getElementById("loBtn")
+const NrGuessedBtn = document.getElementById("gsBtn")
+const Text = document.getElementById("text")
+const Range= document.getElementById("range")
 let guessNr = 500;
 let min = 0;
 let max = 1000;
-StartBtn.addEventListener("click",StartGame);
-HigherBtn.addEventListener("click",GoHigher);
-LowerBtn.addEventListener("click",GoLower);
+
+
+StartBtn.addEventListener("click",StartGame)
+HigherBtn.addEventListener("click",GoHigher)
+LowerBtn.addEventListener("click",GoLower)
+NrGuessedBtn.addEventListener("click",StartGame)
 
 function StartGame(){
-    StartBtn.classList.add("hide");
-    Guess.classList.remove("hide");
-    HiLoContainer.classList.remove("hide");
+    min = 0;
+    max = 1000;
+    guessNr = Average();
+    Guess.innerHTML = guessNr
+    StartBtn.classList.toggle("hide")
+    Guess.classList.toggle("hide")
+    HiLoContainer.classList.toggle("hide")
+    Range.classList.toggle("hide")
 }
 
 
 function GoHigher(){
     min = guessNr+1;
-    guessNr = (min+max)/2;
-    guessNr = Math.round(guessNr);
+    guessNr = Average();
+    guessNr = Math.floor(guessNr);
     Guess.innerHTML = guessNr
 }
 function GoLower(){
     max = guessNr - 1;
-    guessNr = (min+max)/2;
-    guessNr = Math.round(guessNr);
+    guessNr = Average();
+    guessNr = Math.ceil(guessNr);
     Guess.innerHTML = guessNr 
+}
+
+function Average(){
+    return (min+max)/2;
 }

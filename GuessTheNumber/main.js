11c9ebc4fb374bf6d7a026177f5379ca55929363
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2020-11-08 19:29:35 +0200

    Small Update Guess the number

--- GuessTheNumber/main.js
+++ GuessTheNumber/main.js
@@ -1,3 +1,5 @@
+const THINK_TEXT = "Think of an number between"
+const CHOOSE_TEXT = "Is this your number?"
 const StartBtn = document.getElementById("startBtn")
 const Guess = document.getElementById("guess")
 const HiLoContainer = document.getElementById("HiLoContainer")
@@ -6,6 +8,7 @@ const LowerBtn = document.getElementById("loBtn")
 const NrGuessedBtn = document.getElementById("gsBtn")
 const Text = document.getElementById("text")
 const Range= document.getElementById("range")
+let playing = false 
 let guessNr = 500;
 let min = 0;
 let max = 1000;
@@ -17,15 +20,38 @@ LowerBtn.addEventListener("click",GoLower)
 NrGuessedBtn.addEventListener("click",StartGame)
 
 function StartGame(){
-    min = 0;
-    max = 1000;
+    let MinValue = document.getElementById("minValue");
+    let MaxValue = document.getElementById("maxValue");
+    min = parseInt(MinValue.innerHTML)
+    max = parseInt(MaxValue.innerHTML)
+    if(!VerifyMinMaxValues()){
+        min=0
+        max=1000
+        MinValue.innerHTML = min
+        MaxValue.innerHTML = max
+        alert("Write valid min max values")
+        return;
+    }
     guessNr = Average();
     Guess.innerHTML = guessNr
     StartBtn.classList.toggle("hide")
     Guess.classList.toggle("hide")
     HiLoContainer.classList.toggle("hide")
     Range.classList.toggle("hide")
+    if(playing)
+        Text.innerHTML = THINK_TEXT;
+    else
+        Text.innerHTML = CHOOSE_TEXT;
+    playing=!playing;
 }
+function VerifyMinMaxValues(){
+    if(min>=max)
+        return false
+    if(isNaN(min) || isNaN(max))
+        return false
+    return true
+}
+
 
 
 function GoHigher(){
